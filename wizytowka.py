59e0e425f0b8505faac071756aa317cabd57ad82
Rafał Raźny <rafal-razny@wp.pl>
2022-01-23 14:38:33 +0100

    Change import wizytowka.py

--- wizytowka.py
+++ wizytowka.py
@@ -1,7 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
 from flask import render_template
 from flask import request, redirect
+app = Flask(__name__)
 
 @app.route("/mypage/me")
 def mypage():
